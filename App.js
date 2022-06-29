cfb1c39cdccd5d6bd9cb14aa16ce5b7b6f5461ef
nikhilaasnikar1 <nikhil.aasnikar@ie.ey.com>
2022-06-29 11:07:40 +0100

    changes

--- App.js
+++ App.js
@@ -20,6 +20,8 @@ const addGoalHandler=()=>{
       <TextInput placeholder='Course Goal' style={styles.input} onChangeText={goalInputHandler} value={enteredGoal}/>
       <Button title='ADD'  onPress={addGoalHandler}/>
       </View>
+      {enteredGoal ? <Text style={styles.card}>{enteredGoal}</Text> : null}
+      
       <View>
       </View>
     </View>
@@ -32,14 +34,24 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: 'row',
-    justifyContent:'space-between',
+    // justifyContent:"space-evenly",
     alignItems:'center'
   },
   input: {
-    width:'80%',
+    width:'50%',
     borderColor: 'black',
     borderWidth: 1,
-    padding: 10
+    padding: 10,
+    margin:10
+  },
+  card: {
+    width:'50%',
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor:'#5261ac',
+    height:50,
+    padding: 10,
+    margin:10
   }
 });
 
